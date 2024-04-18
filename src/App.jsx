3184a15e4b48b1b74ba6250fa33dd9a8d9902623
Krish Bhatt <Krishbhatt1217@gmail.com>
2024-04-18 15:43:49 +0530

    Basic React files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Login from './components/login/Login'
+import SignUp from './components/signup/SignUp'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
